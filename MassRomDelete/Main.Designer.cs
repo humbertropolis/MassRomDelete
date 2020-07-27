173323f1d45e795ba71a986c92976a9b31282a20
Andres H Agredo <humbertropolis@hotmail.com>
2020-07-27 03:21:34 -0500

    Basic delete operation based on Name and Genre

--- MassRomDelete/Main.Designer.cs
+++ MassRomDelete/Main.Designer.cs
@@ -29,37 +29,25 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.txtRomsFolder = new System.Windows.Forms.TextBox();
-            this.txtImagesFolder = new System.Windows.Forms.TextBox();
             this.btnRomsFolder = new System.Windows.Forms.Button();
-            this.btnImagesFolder = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.txtNameFilter = new System.Windows.Forms.TextBox();
             this.txtGenreFilter = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
             this.romsFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.imagesFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.cbNameFilter = new System.Windows.Forms.CheckBox();
+            this.cbGenreFilter = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Location = new System.Drawing.Point(2, 23);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Roms Folder";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 49);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
-            this.label2.TabIndex = 9;
-            this.label2.Text = "Images Folder";
             // 
             // txtRomsFolder
             // 
@@ -67,13 +55,6 @@
             this.txtRomsFolder.Name = "txtRomsFolder";
             this.txtRomsFolder.Size = new System.Drawing.Size(364, 20);
             this.txtRomsFolder.TabIndex = 1;
-            // 
-            // txtImagesFolder
-            // 
-            this.txtImagesFolder.Location = new System.Drawing.Point(91, 47);
-            this.txtImagesFolder.Name = "txtImagesFolder";
-            this.txtImagesFolder.Size = new System.Drawing.Size(364, 20);
-            this.txtImagesFolder.TabIndex = 3;
             // 
             // btnRomsFolder
             // 
@@ -83,59 +64,31 @@
             this.btnRomsFolder.TabIndex = 2;
             this.btnRomsFolder.Text = "...";
             this.btnRomsFolder.UseVisualStyleBackColor = true;
-            this.btnRomsFolder.Click += new System.EventHandler(this.btnRomsFolder_Click);
-            // 
-            // btnImagesFolder
-            // 
-            this.btnImagesFolder.Location = new System.Drawing.Point(461, 45);
-            this.btnImagesFolder.Name = "btnImagesFolder";
-            this.btnImagesFolder.Size = new System.Drawing.Size(25, 20);
-            this.btnImagesFolder.TabIndex = 4;
-            this.btnImagesFolder.Text = "...";
-            this.btnImagesFolder.UseVisualStyleBackColor = true;
-            this.btnImagesFolder.Click += new System.EventHandler(this.btnImagesFolder_Click);
+            this.btnRomsFolder.Click += new System.EventHandler(this.BtnRomsFolder_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(209, 141);
+            this.btnDelete.Location = new System.Drawing.Point(209, 116);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(75, 23);
             this.btnDelete.TabIndex = 7;
             this.btnDelete.Text = "Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
-            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 76);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(60, 13);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "Name Filter";
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
             // 
             // txtNameFilter
             // 
-            this.txtNameFilter.Location = new System.Drawing.Point(91, 73);
+            this.txtNameFilter.Location = new System.Drawing.Point(91, 48);
             this.txtNameFilter.Name = "txtNameFilter";
             this.txtNameFilter.Size = new System.Drawing.Size(364, 20);
-            this.txtNameFilter.TabIndex = 5;
+            this.txtNameFilter.TabIndex = 4;
             // 
             // txtGenreFilter
             // 
-            this.txtGenreFilter.Location = new System.Drawing.Point(91, 99);
+            this.txtGenreFilter.Location = new System.Drawing.Point(91, 74);
             this.txtGenreFilter.Name = "txtGenreFilter";
             this.txtGenreFilter.Size = new System.Drawing.Size(364, 20);
             this.txtGenreFilter.TabIndex = 6;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 102);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(61, 13);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Genre Filter";
             // 
             // romsFolderDialog
             // 
@@ -145,21 +98,38 @@
             // 
             this.imagesFolderDialog.ShowNewFolderButton = false;
             // 
+            // cbNameFilter
+            // 
+            this.cbNameFilter.AutoSize = true;
+            this.cbNameFilter.Location = new System.Drawing.Point(5, 51);
+            this.cbNameFilter.Name = "cbNameFilter";
+            this.cbNameFilter.Size = new System.Drawing.Size(79, 17);
+            this.cbNameFilter.TabIndex = 3;
+            this.cbNameFilter.Text = "Name Filter";
+            this.cbNameFilter.UseVisualStyleBackColor = true;
+            // 
+            // cbGenreFilter
+            // 
+            this.cbGenreFilter.AutoSize = true;
+            this.cbGenreFilter.Location = new System.Drawing.Point(5, 77);
+            this.cbGenreFilter.Name = "cbGenreFilter";
+            this.cbGenreFilter.Size = new System.Drawing.Size(80, 17);
+            this.cbGenreFilter.TabIndex = 5;
+            this.cbGenreFilter.Text = "Genre Filter";
+            this.cbGenreFilter.UseVisualStyleBackColor = true;
+            // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 188);
-            this.Controls.Add(this.label4);
+            this.ClientSize = new System.Drawing.Size(498, 151);
+            this.Controls.Add(this.cbGenreFilter);
+            this.Controls.Add(this.cbNameFilter);
             this.Controls.Add(this.txtGenreFilter);
             this.Controls.Add(this.txtNameFilter);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.btnDelete);
-            this.Controls.Add(this.btnImagesFolder);
             this.Controls.Add(this.btnRomsFolder);
-            this.Controls.Add(this.txtImagesFolder);
             this.Controls.Add(this.txtRomsFolder);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
             this.Name = "Main";
@@ -173,17 +143,14 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtRomsFolder;
-        private System.Windows.Forms.TextBox txtImagesFolder;
         private System.Windows.Forms.Button btnRomsFolder;
-        private System.Windows.Forms.Button btnImagesFolder;
         private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtNameFilter;
         private System.Windows.Forms.TextBox txtGenreFilter;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.FolderBrowserDialog romsFolderDialog;
         private System.Windows.Forms.FolderBrowserDialog imagesFolderDialog;
+        private System.Windows.Forms.CheckBox cbNameFilter;
+        private System.Windows.Forms.CheckBox cbGenreFilter;
     }
 }
--- MassRomDelete/Main.cs
+++ MassRomDelete/Main.cs
@@ -1,17 +1,17 @@
-﻿using System;
+﻿using MassRomDelete.Structures;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace MassRomDelete
 {
     public partial class Main : Form
     {
+        private const string gameListFileName = "gamelist.xml";
+        public string GameListFile { get; set; }
+
         public Main()
         {
             InitializeComponent();
@@ -22,7 +22,21 @@ namespace MassRomDelete
 
         }
 
-        private void btnRomsFolder_Click(object sender, EventArgs e)
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (!ValidateRomsFolder() || !ValidateFilters())
+                return;
+
+            var gameList = DeserializeGameList();
+
+            DeleteFiles(gameList);
+            SerializeGameList(gameList);
+
+            var msg = "Operation successfully";
+            MessageBox.Show(msg);
+        }       
+
+        private void BtnRomsFolder_Click(object sender, EventArgs e)
         {
             if (romsFolderDialog.ShowDialog() == DialogResult.OK)
             {
@@ -30,17 +44,130 @@ namespace MassRomDelete
             }
         }
 
-        private void btnImagesFolder_Click(object sender, EventArgs e)
+        #region Aux Methods
+
+        private void DeleteFiles(GameList gameList)
         {
-            if (imagesFolderDialog.ShowDialog() == DialogResult.OK)
+            var gamesToRemove = new GameList { Games = new List<Game>() };
+            var alreadyRemoved = false;
+
+            foreach (var game in gameList.Games)
             {
-                txtImagesFolder.Text = imagesFolderDialog.SelectedPath;
+                var rom = game.Path.Substring(2, game.Path.Length-2);
+                var image = string.IsNullOrEmpty(game.Image) ? string.Empty : game.Image.Substring(2, game.Image.Length-2);
+
+                if (cbNameFilter.Checked)
+                {
+                    if (game.Name.ToLower().Contains(txtNameFilter.Text.ToLower()))
+                    {
+                        RemoveOperation(gamesToRemove, game, rom, image);
+                        alreadyRemoved = true;
+                    }                        
+                }                    
+
+                if (cbGenreFilter.Checked && !alreadyRemoved)
+                {
+                    if (game.Genre.ToLower().Contains(txtGenreFilter.Text.ToLower()))
+                        RemoveOperation(gamesToRemove, game, rom, image);                    
+                }
+
+                alreadyRemoved = false;
+            }
+
+            foreach (var game in gamesToRemove.Games)
+            {
+                gameList.Games.Remove(game);
+            }
+        }        
+
+        private GameList DeserializeGameList()
+        {
+            var deserializer = new XmlSerializer(typeof(GameList));
+            using (var reader = new StreamReader(GameListFile))
+            {
+                var gameList = (GameList)deserializer.Deserialize(reader);
+
+                return gameList;
             }
         }
 
-        private void btnDelete_Click(object sender, EventArgs e)
+        private void RemoveOperation(GameList gamesToRemove, Game game, string rom, string image)
         {
-
+            File.Delete(Path.Combine(txtRomsFolder.Text, rom));
+            if(!string.IsNullOrEmpty(image))
+                File.Delete(Path.Combine(txtRomsFolder.Text, image));
+            
+            gamesToRemove.Games.Add(game);
         }
+
+        private void SerializeGameList(GameList gameList)
+        {
+            var serializer = new XmlSerializer(typeof(GameList));
+            var streamWriter = new StreamWriter(GameListFile);
+
+            serializer.Serialize(streamWriter, gameList);
+            streamWriter.Close();
+        }
+
+        private bool ValidateFilters()
+        {
+            if (!cbGenreFilter.Checked && !cbNameFilter.Checked)
+            {
+                var msg = "You must select at least one filter";
+                MessageBox.Show(msg);
+
+                return false; 
+            }
+
+
+            var a = ValidateNameFilter() && ValidateGenreFilter();
+            return a;
+        }
+
+        private bool ValidateGenreFilter()
+        {
+            if (cbGenreFilter.Checked && string.IsNullOrEmpty(txtGenreFilter.Text)) 
+            {
+                var msg = "Genre filter selected but field is empty";
+                MessageBox.Show(msg);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateNameFilter()
+        {
+            if (cbNameFilter.Checked && string.IsNullOrEmpty(txtNameFilter.Text))
+            {
+                var msg = "Name filter selected but field is empty";
+                MessageBox.Show(msg);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ValidateRomsFolder()
+        {
+            try
+            {
+                GameListFile = Path.Combine(txtRomsFolder.Text, gameListFileName);                
+                if(!File.Exists(GameListFile) || !Directory.Exists(txtRomsFolder.Text))
+                    throw new Exception();
+            }
+            catch
+            {
+                var msg = "Invalid Roms Folder";
+                MessageBox.Show(msg);
+                return false;
+            }
+
+            return true;
+        }
+        
+        #endregion
     }
 }
